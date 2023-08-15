c8e66f6c556433dafbe24f71630f9aad9beed83d
EnTech <marinoskouvaras@gmail.com>
2023-08-15 12:46:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Find your Account SID and Auth Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        string accountSid = "TWILIO_ACCOUNT_SID";
+        string authToken = "TWILIO_AUTH_TOKEN";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = MessageResource.Create(
+            body: "You have a message from EnTech.",
+            from: new Twilio.Types.PhoneNumber("+XXXXXXXXXXXX"),
+            to: new Twilio.Types.PhoneNumber("+XXXXXXXXXXXX")
+        );
+
+        Console.WriteLine(message.Sid);
+    }
+}
